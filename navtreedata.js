cff3dba33f8d8ca21305ba31052f1bc3dec4ee76
MSylvia <MSylvia@users.noreply.github.com>
2022-09-05 16:26:56 +0000

    deploy: dc405a8c0aa9a5bf9fbe78123aa6bc56d16d3258

---
+++ navtreedata.js
@@ -0,0 +1,98 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Jolt Physics", "index.html", [
+    [ "Architecture of Jolt Physics", "index.html", null ],
+    [ "Jolt Physics Samples", "md__docs__samples.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_a_b_b_tree_builder_8cpp.html",
+"_core_8h.html#a4ac413403e9ca00d24a3e2f244cf5456",
+"_memory_8cpp.html#ae8854fb6cfcc5836d8e9f6103a3a6c68",
+"_r_t_t_i_8h.html#adb200046bbf9e46a9870dc1862e8cc15",
+"_swing_twist_constraint_8cpp.html#a1513293e8c5fb785aa7142d42159c3f2",
+"class_a_a_box.html#af29df5ecf944d7ec92eea7d016e49320",
+"class_body_lock_base.html#a8ee09ef1fb31697b4f965c8e5ee09e5f",
+"class_capsule_shape_1_1_capsule_with_convex.html#ab43fab2b559ee221f84cfff5038aba0d",
+"class_compound_shape.html#a230f2ff5c2d384c55f7845ff18acc8b0",
+"class_convex_hull_shape_settings.html#a3922b9c1f60a1e6b49fd1600304ed9dd",
+"class_e_p_a_convex_hull_builder.html#aba8a5ac94c67e4c96e5eb533dfca3a83",
+"class_hinge_constraint.html#ab511231f1d1c0ea20d9d7e9a3d996091",
+"class_mat44.html#a3be30e0ae2ded68ebb5ceea9afbfe5c1",
+"class_object_stream_binary_in.html#aaf2b6afb5aa25ca74c4505973c0a48f7",
+"class_physics_system.html#aadad532dfda2006fd3b699116e9fed08",
+"class_ragdoll.html#a5e5db13680f6f1c62cc5fb4d21ea367f",
+"class_serializable_attribute.html#a0709b27ded07a2d1baab744a317d576e",
+"class_slider_constraint.html#a312b98bfdc4c378cdbabdbd7c34e5458",
+"class_swing_twist_constraint.html#a96e0594e40bc6d63d72241e03cdebcc0",
+"class_triangle_splitter_binning.html#a528500c9d8c6ba8225ec945e135c9588",
+"class_vec8.html#a5b527ce885b1b22c56173d4e48425bb3",
+"class_wheel_w_v.html#af4a4bb2b7d6fbc7100be7a6b9d189ea7",
+"struct_body_pair.html#a20493f29c36b3f20e8db0e86849e1547",
+"struct_physics_settings.html#ae1969d17d76a46e36471e1289d02355f"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
